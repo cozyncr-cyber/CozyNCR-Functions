ce821d57afa377e95958c261c0413f9044b57b4d
cozyncr-cyber <cozyncr@gmail.com>
2026-02-12 10:45:10 +0530

    .

--- send-push-notifications/index.js
+++ send-push-notifications/index.js
@@ -42,7 +42,7 @@ export default async ({ req, res, log, error }) => {
   }
 
     // 2. Fetch push tokens
-    const tokenPath = `/databases/${DATABASE_ID}/collections/push_tokens/documents}`;
+    const tokenPath = `/databases/${DATABASE_ID}/collections/push_tokens/documents`;
     const tokenRes = await appwriteFetch(tokenPath);
     const tokenData = await tokenRes.json();
 // This will now print the actual data instead of [object Object]
