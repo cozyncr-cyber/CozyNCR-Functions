13ed8c8281702432b9a0c79e10f7f1c7a21eb5c3
cozyncr-cyber <cozyncr@gmail.com>
2026-02-21 14:07:38 +0530

    updated variable

--- send-push-notifications/index.js
+++ send-push-notifications/index.js
@@ -43,7 +43,7 @@ export default async ({ req, res, log, error }) => {
     }
     
     // 2. Fetch push tokens
-    const tokenPath = `/databases/${DATABASE_ID}/collections/${PUSH_TOKENS_COLLECTION}/documents`;
+    const tokenPath = `/databases/${DATABASE_ID}/collections/push_tokens/documents`;
     const tokenRes = await appwriteFetch(tokenPath);
     const tokenData = await tokenRes.json();
 // This will now print the actual data instead of [object Object]
